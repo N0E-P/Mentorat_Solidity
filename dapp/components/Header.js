b42cc88659bf82c3bf3d6fe2078e5eaf71e85577
N0E-P <pecci.noe@gmail.com>
2022-08-08 11:08:17 +0200

    tailwindcss

--- dapp/components/Header.js
+++ dapp/components/Header.js
@@ -2,7 +2,7 @@ import { ConnectButton } from "web3uikit"
 
 export default function Header() {
 	return (
-		<nav className="p-5 border-b2 flex flex-row justify-between items-center">
+		<nav className="p-5 border-b2 flex flex-row justify-between items-center shadow-md shadow-gray-400">
 			<h1 className="py-4 px-4 font-bold text-3xl">NFT Dapp - Mentorat Solidity</h1>
 			<div className="flex flex-row items-center">
 				<ConnectButton />
--- dapp/components/Main.js
+++ dapp/components/Main.js
@@ -5,7 +5,7 @@ export default function Main() {
 	const contractAddresses = require("../constants/contractAddresses.json")
 	const contractAddress = contractAddresses["4"][0]
 	const abi = require("../constants/abi.json")
-	const dispatch = useNotification()
+	const notification = useNotification()
 
 	const {
 		runContractFunction: mint,
@@ -16,13 +16,12 @@ export default function Main() {
 		abi: abi,
 		contractAddress: contractAddress,
 		functionName: "mint",
-		//msgValue: 0, //////////////////////////////
 		params: {},
 	})
 
 	const handleSuccess = async (tx) => {
 		await tx.wait(1)
-		dispatch({
+		notification({
 			type: "info",
 			message: "Transaction Complete!",
 			title: "Transaction Notification",
@@ -32,25 +31,31 @@ export default function Main() {
 	}
 
 	return (
-		<div className="p-5">
-			<h1 className="py-4 px-4 font-bold text-3xl">Mint an NFT</h1>
-			<p>Make sure to connect your Wallet to the Rinkeby Network</p>
-			<button
-				className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-auto"
-				onClick={async () =>
-					await mint({
-						onSuccess: handleSuccess,
-						onError: (error) => console.log(error),
-					})
-				}
-				disabled={isLoading || isFetching}
-			>
-				{isLoading || isFetching ? (
-					<div className="animate-spin spinner-border h-8 w-8 border-b-2 rounded-full"></div>
-				) : (
-					"Mint Here!"
-				)}
-			</button>
+		<div className="py-24">
+			<div className="h-auto m-auto max-w-[600px] shadow-lg shadow-gray-400 rounded-xl items-center justify-center p-4 ">
+				<h1 className="py-2 px-4 font-bold text-3xl">Mint an NFT</h1>
+				<p className="py-3 px-4">
+					Make sure to connect your Wallet to the Rinkeby Network.
+				</p>
+				<div className="px-52">
+					<button
+						className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded shadow-md shadow-gray-400 ml-auto hover:scale-105 ease-in duration-300"
+						onClick={async () =>
+							await mint({
+								onSuccess: handleSuccess,
+								onError: (error) => console.log(error),
+							})
+						}
+						disabled={isLoading || isFetching}
+					>
+						{isLoading || isFetching ? (
+							<div className="animate-spin spinner-border h-8 w-8 border-b-2 rounded-full"></div>
+						) : (
+							"Mint Here!"
+						)}
+					</button>
+				</div>
+			</div>
 		</div>
 	)
 }
